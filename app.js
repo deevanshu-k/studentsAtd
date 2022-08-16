102f8953e3bdc8d16365966a0c788d05c4ad180d
Deevanshu kushwah <deevanshukushwah80@gmail.com>
2022-08-16 23:47:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+var express = require('express');
+var app = express();
+const ejs = require("ejs");
+const path = require("path");
+
+
+var port = 80;
+var host = '127.0.0.1'
+
+
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+// network routs
+app.get('/', (req, res) => {
+  
+    res.status(200).send('Hello world!');
+});
+
+
+
+// Error Handelling
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+ })
+ 
+ app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+       error: {
+          message: error.message
+       }
+    });
+ })
+
+//  Listening
+var server = app.listen(process.env.port, function () {
+   console.log("Example app listening at http://%s:%s/",host, process.env.port)
+})
